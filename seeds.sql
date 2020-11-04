b872cf92a9e4c000c7987bd90b9271d58bf72b8b
gabenozea <64702460+gabenozea@users.noreply.github.com>
2020-11-04 00:01:06 -0500

    done

---
+++ seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Meat Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Diet Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegetable Burger', FALSE);
